8f2a676eed1db7f969767c25040ede7654852b87
mamokolo <like286bp6show@gmail.com>
2021-03-11 18:12:54 +0800

    imageBtn fix

--- app/src/main/java/com/example/newstuff/GameFragment.java
+++ app/src/main/java/com/example/newstuff/GameFragment.java
@@ -3,24 +3,19 @@ package com.example.newstuff;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
-import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.AppCompatImageView;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.Navigation;
 
 import android.util.DisplayMetrics;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -32,7 +27,7 @@ public class GameFragment extends Fragment {
     private Activity activity;
     private TextView score,time;
     private Button pause;
-    private ImageButton turnLeft,turnRight,flash;
+    private ImageButton turnLeftbtn,turnRightbtn,skillBtn;
     private ImageView godtone;
     private int width,height,change;
     private int relatedPosition=0;
@@ -61,6 +56,7 @@ public class GameFragment extends Fragment {
         activity = getActivity();
         
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -73,6 +69,7 @@ public class GameFragment extends Fragment {
 
         System.out.println(width+" "+height);
     }
+
 
 
     @Override
@@ -90,16 +87,61 @@ public class GameFragment extends Fragment {
         score = view.findViewById(R.id.textScore);
         time = view.findViewById(R.id.textTime);
         pause = view.findViewById(R.id.pauseBtn);
-        turnLeft = view.findViewById(R.id.imageButtonLeft);
-        turnRight = view.findViewById(R.id.imageButtonRight);
-        flash = view.findViewById(R.id.imageButtonFlash);
+        turnLeftbtn = view.findViewById(R.id.imageButtonLeft);
+        turnRightbtn = view.findViewById(R.id.imageButtonRight);
+        skillBtn = view.findViewById(R.id.imageButtonSkill);
         godtone = view.findViewById(R.id.imageGodtone);
         change = width/6;
+
+        if(bundle!=null){
+            if(bundle.getString("skill").equals("flash")){
+                System.out.println("flash");
+                skillBtn.setImageDrawable(getResources().getDrawable(R.drawable.flash));
+            }
+            else if(bundle.getString("skill").equals("heal")){
+                System.out.println("heal");
+                skillBtn.setImageDrawable(getResources().getDrawable(R.drawable.heal));
+            }
+            else if(bundle.get("skill").equals("shield")){
+                System.out.println("shield");
+                skillBtn.setImageDrawable(getResources().getDrawable(R.drawable.shield));
+            }
+            else{
+                System.out.println("Worng");
+                AlertDialog.Builder wrongDialog = new AlertDialog.Builder(activity);
+                wrongDialog.setTitle("SOMETHING WRONG");
+                wrongDialog.setMessage(getString(R.string.textSkillWrong));
+                wrongDialog.setCancelable(false);
+                wrongDialog.setNeutralButton(getString(R.string.textOK), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        bundle.remove("skill");
+                        Navigation.findNavController(view).navigate(R.id.resultFragment,bundle);
+                    }
+                });
+                wrongDialog.show();
+            }
+        }
+        else{
+            AlertDialog.Builder bundleNullDialog = new AlertDialog.Builder(activity);
+            bundleNullDialog.setTitle("SOMETHING WRONG");
+            bundleNullDialog.setMessage(getString(R.string.textBundleNull));
+            bundleNullDialog.setCancelable(false);
+            bundleNullDialog.setNeutralButton(getString(R.string.textOK), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    bundle.clear();
+                    Navigation.findNavController(view).navigate(R.id.mainFragment);
+                }
+            });
+            bundleNullDialog.show();
+        }
+
 
         Animation turnLeftAnim = new TranslateAnimation(godtone.getX(),godtone.getX()-change,godtone.getY(),godtone.getY());
         Animation turnRightAnim = new TranslateAnimation(godtone.getX(),godtone.getX()+change,godtone.getY(),godtone.getY());
 
-        turnLeft.setOnClickListener(new View.OnClickListener() {
+        turnLeftbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(relatedPosition>-2){
@@ -109,11 +151,9 @@ public class GameFragment extends Fragment {
                     System.out.println("turn left");
                     relatedPosition -= 1;
                 }
-                else {
-                }
             }
         });
-        turnRight.setOnClickListener(new View.OnClickListener() {
+        turnRightbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(relatedPosition<2){godtone.setAnimation(turnRightAnim);
@@ -122,15 +162,13 @@ public class GameFragment extends Fragment {
                     System.out.println("turn right");
                     relatedPosition += 1;
                 }
-                else {
-                }
             }
         });
-        flash.setOnClickListener(new View.OnClickListener() {
+        skillBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //godtone.setAnimation(AnimationUtils.loadAnimation(activity,R.anim.anim_turn_left));
-                System.out.println("use flash");
+                System.out.println("use skill");
             }
         });
 
